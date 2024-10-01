e4ffca3eb468726e1adbfedb59bba53e8cb84400
pradipbhatt <bhattsammar04@gmail.com>
2024-10-01 08:06:48 +0545

    Init commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -46,40 +46,45 @@ const Banner = () => {
         <h2 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-black mt-4 mb-8 text-[#00274D]">
           Colleges for +2 in Far West, Nepal.
         </h2>
-        <div className="w-full max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-center gap-4 mb-4 ">
-          <select
-            className="p-3 bg-[#00274D] text-white font-semibold rounded-lg outline-none flex-grow mb-4 md:mb-0 md:mr-2 sm:w-1/2"
-            onChange={(e) => setSelectedSchool(e.target.value)}
-          >
-            <option  className='bg-white text-black' value="">Select College</option>
-            <option className='bg-white text-black' value="Radiant Secondary School">Radiant Secondary School</option>
-            <option className='bg-white text-black' value="LBA">LBA</option>
-            <option className='bg-white text-black' value="SPA">SPA</option>
-            <option className='bg-white text-black' value="Sadharan mavi">Sadharan Secondary School</option>
-            <option  className='bg-white text-black' value="Morning Glory">Morning Glory</option>
-          </select>
-          <input
-            type="text"
-            placeholder="Enter the name of the school"
-            value={selectedSchool}
-            onChange={(e) => setSelectedSchool(e.target.value)}
-            className="p-3 text-gray-600 outline-none flex-grow mb-4 md:mb-0 md:mr-2 sm:w-full"
-            style={{
-              borderRadius: '0.5rem',
-              boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-            }}
-          />
-          <button
-            onClick={handleFind}
-            className="p-3 bg-[#00274D] text-white font-semibold rounded-lg w-full md:w-auto"
-            style={{
-              borderRadius: '0.5rem',
-              boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-            }}
-          >
-            Search
-          </button>
-        </div>
+        <div className="w-full max-w-4xl mx-auto flex flex-col sm:flex-row items-center justify-center gap-4 mb-4">
+  <div className="flex-grow mb-4 sm:mb-0 sm:mr-2">
+    <select
+      className="p-3 bg-[#00274D] text-white font-semibold rounded-lg outline-none w-full"
+      onChange={(e) => setSelectedSchool(e.target.value)}
+    >
+      <option className='bg-white text-black' value="">Select College</option>
+      <option className='bg-white text-black' value="Radiant Secondary School">Radiant Secondary School</option>
+      <option className='bg-white text-black' value="LBA">LBA</option>
+      <option className='bg-white text-black' value="SPA">SPA</option>
+      <option className='bg-white text-black' value="Sadharan mavi">Sadharan Secondary School</option>
+      <option className='bg-white text-black' value="Morning Glory">Morning Glory</option>
+    </select>
+  </div>
+  <div className="flex-grow mb-4 sm:mb-0 sm:mr-2">
+    <input
+      type="text"
+      placeholder="Enter the name of the school"
+      value={selectedSchool}
+      onChange={(e) => setSelectedSchool(e.target.value)}
+      className="p-3 text-gray-600 outline-none w-full"
+      style={{
+        borderRadius: '0.5rem',
+        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+      }}
+    />
+  </div>
+  <button
+    onClick={handleFind}
+    className="p-3 bg-[#00274D] text-white font-semibold rounded-lg w-full sm:w-auto"
+    style={{
+      borderRadius: '0.5rem',
+      boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+    }}
+  >
+    Search
+  </button>
+</div>
+
       </div>
     </div>
   );
